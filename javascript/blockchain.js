e2cfe84482850e63b3b18bef5c21ce7c63e03194
SamuelMSD <56126328+SamuelMSD@users.noreply.github.com>
2019-12-02 19:29:38 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -3,3 +3,4 @@ var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
 var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
+
